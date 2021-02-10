85238177c309f2a615cbf15b2706b25c0e3fd55a
Yusril A. P <yusril_putra56@yahoo.co.id>
2021-02-10 15:27:29 +0700

    Fix some typo on indonesian about page

--- src/scripts/data/about.js
+++ src/scripts/data/about.js
@@ -7,7 +7,7 @@ const aboutConfig = {
     en:
     'Im a college student at State University of Surabaya. I love learning about the world of technology especially on the website development with javascript. My programming experience now is approximately 1 year.',
     id:
-      'Saya adalah seorang mahasiswa Sistem Informasi di Universitas Negeri Surabaya. Saya memiliki semangat dan antusias yang tingi dalam mendalami bidang pengembangan website dengan javascript. Pengalaman pada bidang pengembangan website kurang lebih 1 tahun.',
+      'Saya adalah seorang mahasiswa Sistem Informasi di Universitas Negeri Surabaya. Saya memiliki semangat dan antusias yang tinggi dalam mendalami bidang pengembangan website dengan javascript. Pengalaman pada bidang pengembangan website kurang lebih 1 tahun.',
   },
   decoration: {
     aria_label: {
