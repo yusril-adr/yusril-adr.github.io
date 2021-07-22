3eab4d3566ba4408fb79a7ab250e0d489a384d60
Yusril A. P <yusriladr.37@gmail.com>
2021-07-22 15:43:27 +0700

    Implement lazy load

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import '../styles/style.scss';
 import App from './views/app';
 
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -14,7 +14,7 @@ const createFrameworkItemTemplate = ({
   name, url, image: { src, alt },
 }, lang) => `
   <a href="${url}" target="_blank" rel="noreferrer" class="text-center text-white hover:text-blue-400">
-    <img src="${src}" alt="${alt[lang]}" class="h-16 m-auto">
+    <img data-src="${src}" alt="${alt[lang]}" class="lazyload h-16 m-auto">
 
     <p class="mt-6">${name}</p>
   </a>
@@ -35,14 +35,19 @@ const createOddPortfolioTemplate = ({
 
   <a href="${url}" target="_blank" rel="noreferrer" class="contents">
     <picture class="portfolio-image md:inline-block">
-      <source media="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px)" srcset="/images/${image.name}-small.webp">
-      <source media="(max-width: ${image.size - 1}px)" srcset="/images/${image.name}-medium.webp">
-      <source srcset="/images/${image.name}.webp">
-      <img src="/images/${image.name}.png" srcset="
-            /images/${image.name}-small.png ${CONFIG.PORTFOLIO.IMAGES.SMALL}w,
-            /images/${image.name}-medium.png ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}w,
-            /images/${image.name}.png ${image.size}w
-        " sizes="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px) ${CONFIG.PORTFOLIO.IMAGES.SMALL}px,(max-width: ${image.size - 1}px) ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}px, ${image.size}px" alt="${name}" />
+      <source media="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px)" data-srcset="/images/${image.name}-small.webp">
+      <source media="(max-width: ${image.size - 1}px)" data-srcset="/images/${image.name}-medium.webp">
+      <source data-srcset="/images/${image.name}.webp">
+      <img 
+        class="lazyload"
+        data-src="/images/${image.name}.png" 
+        data-srcset="
+          /images/${image.name}-small.png ${CONFIG.PORTFOLIO.IMAGES.SMALL}w,
+          /images/${image.name}-medium.png ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}w,
+          /images/${image.name}.png ${image.size}w
+        "
+        data-sizes="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px) ${CONFIG.PORTFOLIO.IMAGES.SMALL}px,(max-width: ${image.size - 1}px) ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}px, ${image.size}px" alt="${name}"
+      />
 
       <div class="hover-image">
         <i class="fas fa-search m-auto text-gradient text-7xl" aria-label="${aria_label[lang]}"></i>
@@ -56,14 +61,19 @@ const createEvenPortfolioTemplate = ({
 }, lang) => `
   <a href="${url}" target="_blank" rel="noreferrer" class="contents">
     <picture class="portfolio-image md:inline-block">
-      <source media="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px)" srcset="/images/${image.name}-small.webp">
-      <source media="(max-width: ${image.size - 1}px)" srcset="/images/${image.name}-medium.webp">
-      <source srcset="/images/${image.name}.webp">
-      <img src="/images/${image.name}.png" srcset="
-              /images/${image.name}-small.png ${CONFIG.PORTFOLIO.IMAGES.SMALL}w,
-              /images/${image.name}-medium.png ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}w,
-              /images/${image.name}.png ${image.size}w
-          " sizes="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px) ${CONFIG.PORTFOLIO.IMAGES.SMALL}px,(max-width: ${image.size - 1}px) ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}px, ${image.size}px" alt="${name}" />
+      <source media="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px)" data-srcset="/images/${image.name}-small.webp">
+      <source media="(max-width: ${image.size - 1}px)" data-srcset="/images/${image.name}-medium.webp">
+      <source data-srcset="/images/${image.name}.webp">
+      <img 
+        class="lazyload"
+        data-src="/images/${image.name}.png" 
+        data-srcset="
+          /images/${image.name}-small.png ${CONFIG.PORTFOLIO.IMAGES.SMALL}w,
+          /images/${image.name}-medium.png ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}w,
+          /images/${image.name}.png ${image.size}w
+        " 
+        data-sizes="(max-width: ${CONFIG.PORTFOLIO.IMAGES.SMALL}px) ${CONFIG.PORTFOLIO.IMAGES.SMALL}px,(max-width: ${image.size - 1}px) ${CONFIG.PORTFOLIO.IMAGES.MEDIUM}px, ${image.size}px" alt="${name}"
+      />
 
       <div class="hover-image">
         <i class="fas fa-search m-auto text-gradient text-7xl" aria-label="${aria_label[lang]}"></i>
