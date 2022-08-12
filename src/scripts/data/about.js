aadb5690ccbc303ee5758b986b7d2be8e489634d
Yusril Adriansyah P <yusriladr.37@gmail.com>
2022-08-12 14:04:24 +0700

    Update about me description

--- src/scripts/data/about.js
+++ src/scripts/data/about.js
@@ -5,9 +5,9 @@ const aboutConfig = {
   },
   description: {
     en:
-    'Im a Junior Web Developer with approximately 2 years. Experienced in web development using MySQL and MongoDB as database. Also experiencded using Javascript framework tools like NodeJS, ReactJS, Express. Currently focus on developing web app using API and Single-Page App approach with MERN Stack which is stand for MongoDB Express ReactJS NodeJS.',
+    'Im a Junior Javascript Web Developer. Experienced in web development using MySQL and MongoDB as database. Also experiencded using Javascript framework tools like NodeJS, ReactJS, Express. Currently focus on developing web app using API and Single-Page App approach with MERN Stack which is stand for MongoDB Express ReactJS NodeJS.',
     id:
-      'Saya adalah seorang Junior Web Developer selama kurang lebih 2 tahun. Berpengalaman dalam pengembangan web menggunakan MySQL dan MongoDB sebagai database. Dan juga berpengalaman menggunakan framework Javascript seperti NodeJS, ReactJS, ExpressJS. Saat ini fokus pada pengembangan web menggunakan pendekatan API dan Single-Page App dengan MERN Stack yang merupakan singkatan dari MongoDB Express ReactJS NodeJS.',
+      'Saya adalah seorang Junior Javascript Web Developer. Berpengalaman dalam pengembangan web menggunakan MySQL dan MongoDB sebagai database. Dan juga berpengalaman menggunakan framework Javascript seperti NodeJS, ReactJS, ExpressJS. Saat ini fokus pada pengembangan web menggunakan pendekatan API dan Single-Page App dengan MERN Stack yang merupakan singkatan dari MongoDB Express ReactJS NodeJS.',
   },
   decoration: {
     aria_label: {
